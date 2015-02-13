53e6d7ba4976361ace744ee405d3c862195af531
Kira McCoan <kira.mccoan@alumni.ubc.ca>
2015-02-12 22:20:06 -0800

    Missing locale, and seeds refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create!(email: 'kira@example.com', password: 'password', password_confirmation: 'password')
-
 weather = CategoryType.create(name: "Weather")
 
-sun = Category.create(name: "Sun", photoUrl: "sun-icon.png")
-sun.category_type = weather
-sun.save
-
-rain = Category.create(name: "Rain", photoUrl: "rain-icon.png")
-rain.category_type = weather
-rain.save
+rain = weather.categories.create(name: "Rain", photoUrl: "rain-icon.png")
 
 aquarium = rain.activities.create(
 	name:"visit the Aquarium",
@@ -24,8 +16,6 @@ aquarium = rain.activities.create(
 	link: "http://www.vanaqua.org/",
 	lat: 49.300165,
 	long: -123.130978)
-aquarium.categories << rain
-aquarium.save
 
 indoor_climbing = rain.activities.create(
 	name:"Indoor Climbing",
@@ -33,8 +23,8 @@ indoor_climbing = rain.activities.create(
 	link: "http://www.hiveclimbing.com/",
 	lat: 49.261226 ,
 	long: -123.113927)
-indoor_climbing.categories << rain
-indoor_climbing.save
+
+sun = weather.categories.create(name: "Sun", photoUrl: "sun-icon.png")
 
 cliff_jumping = sun.activities.create(
 	name:"Cliff Jumping",
@@ -42,5 +32,3 @@ cliff_jumping = sun.activities.create(
 	link: "http://www.mowgli.ca/zones/lionsbay.html",
 	lat: 49.444859,
 	long: -123.239501)
-cliff_jumping.categories << sun
-cliff_jumping.save
