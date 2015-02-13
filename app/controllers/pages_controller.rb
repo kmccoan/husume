cff9196b03dbbeda320b1cf653e743137468f9d1
Kira McCoan <kira.mccoan@alumni.ubc.ca>
2015-02-12 22:23:13 -0800

    simplify query to check for categories types that have no activities

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def main
-  	@category_ids_with_activities = Category.joins(:activities).pluck(:id).uniq
-  	@category_types = Kaminari.paginate_array(CategoryType.joins(:categories).where(categories: {id: @category_ids_with_activities}).uniq).page(params[:page]).per(1)
+  	@category_types = Kaminari.paginate_array(CategoryType.joins(:activities).uniq).page(params[:page]).per(1)
   end
 end
--- app/models/category_type.rb
+++ app/models/category_type.rb
@@ -3,6 +3,6 @@ class CategoryType < ActiveRecord::Base
 	validates_uniqueness_of :name
 
 	has_many :categories
-
+	has_many :activities, through: :categories
 	default_scope { order(name: :asc) }
 end
