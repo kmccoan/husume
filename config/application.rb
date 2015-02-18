1a3ec868966966dbefc029d1876efd85c453190c
Kira McCoan <kira.mccoan@alumni.ubc.ca>
2015-02-17 20:55:25 -0800

    Fix font on production

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module VancouverProj
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
     config.generators do |g| 
         g.test_framework :rspec,
             :fixtures => true,
